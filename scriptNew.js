4d557644197a9a60e878e5402bb287fc4e82582e
kavinduharshamal <122147919+kavinduharshamal@users.noreply.github.com>
2024-10-03 12:17:35 +0530

    Update scriptNew.js

--- scriptNew.js
+++ scriptNew.js
@@ -8,11 +8,19 @@ import { promises as fsPromises } from "fs"; // Async file operations
 import * as fsSync from "fs"; // Sync file operations like existsSync
 import { fileURLToPath } from "url";
 import multer from "multer"; // For handling file uploads
+import OpenAI from "openai/index.mjs";
+import { promises as fs } from "fs";
 
 dotenv.config();
 
+const openai = new OpenAI({
+  apiKey:
+    "sk-tYrg6PAH8-T5J1kY0oBnraQVv_60NPIdzJzizP1lnST3BlbkFJEQyf3QkoJJ-bwiwRfMxfcxD5RHym8kLRJ00nbmBRAA", // Your OpenAI API key here, I used "-" to avoid errors when the key is not set but you should not do that
+});
+
 const elevenLabsApiKey = "eff3e54a7313f68f0b718d930621b3e1"; // Your ElevenLabs API key
 const voiceID = "5Q0t7uMcjvnagumLfvZi"; // Your ElevenLabs Voice ID
+const voiceIDFemale = "EXAVITQu4vr4xnSDxMaL";
 
 const app = express();
 app.use(express.json());
@@ -299,6 +307,140 @@ app.get("/countPngFiles", async (req, res) => {
     res.status(500).send({ error: "Internal server error" });
   }
 });
+
+////chat gpt lineup
+app.get("/voices", async (req, res) => {
+  res.send(await voice.getVoices(elevenLabsApiKey));
+});
+
+const execCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) reject(error);
+      resolve(stdout);
+    });
+  });
+};
+
+const lipSyncMessage = async (message) => {
+  const time = new Date().getTime();
+  console.log(`Starting conversion for message ${message}`);
+  await execCommand(
+    `ffmpeg -y -i audios/message_${message}.mp3 audios/message_${message}.wav`
+    // -y to overwrite the file
+  );
+  console.log(`Conversion done in ${new Date().getTime() - time}ms`);
+  await execCommand(
+    `./Rhubarb-Lip/rhubarb -f json -o audios/message_${message}.json audios/message_${message}.wav -r phonetic`
+  );
+  // -r phonetic is faster but less accurate
+  console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
+};
+
+app.post("/chat", async (req, res) => {
+  const userMessage = req.body.message;
+  if (!userMessage) {
+    res.send({
+      messages: [
+        {
+          text: "Hey dear... How was your day?",
+          audio: await audioFileToBase64("audios/intro_0.wav"),
+          lipsync: await readJsonTranscript("audios/intro_0.json"),
+          facialExpression: "smile",
+          animation: "Talking_1",
+        },
+        {
+          text: "I missed you so much... Please don't go for so long!",
+          audio: await audioFileToBase64("audios/intro_1.wav"),
+          lipsync: await readJsonTranscript("audios/intro_1.json"),
+          facialExpression: "sad",
+          animation: "Crying",
+        },
+      ],
+    });
+    return;
+  }
+  if (!elevenLabsApiKey || openai.apiKey === "-") {
+    res.send({
+      messages: [
+        {
+          text: "Please my dear, don't forget to add your API keys!",
+          audio: await audioFileToBase64("audios/api_0.wav"),
+          lipsync: await readJsonTranscript("audios/api_0.json"),
+          facialExpression: "angry",
+          animation: "Angry",
+        },
+        {
+          text: "You don't want to ruin Wawa Sensei with a crazy ChatGPT and ElevenLabs bill, right?",
+          audio: await audioFileToBase64("audios/api_1.wav"),
+          lipsync: await readJsonTranscript("audios/api_1.json"),
+          facialExpression: "smile",
+          animation: "Laughing",
+        },
+      ],
+    });
+    return;
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-1106",
+    max_tokens: 1000,
+    temperature: 0.6,
+    response_format: {
+      type: "json_object",
+    },
+    messages: [
+      {
+        role: "system",
+        content: `
+        You are a virtual girlfriend.
+        You will always reply with a JSON array of messages. With a maximum of 20 messages.
+        Each message has a text, facialExpression, and animation property.
+        The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
+        The different animations are: Talking_0, Talking_1, Talking_2, Crying, Laughing, Rumba, Idle, Terrified, and Angry. 
+        `,
+      },
+      {
+        role: "user",
+        content: userMessage || "Hello",
+      },
+    ],
+  });
+  let messages = JSON.parse(completion.choices[0].message.content);
+  if (messages.messages) {
+    messages = messages.messages; // ChatGPT is not 100% reliable, sometimes it directly returns an array and sometimes a JSON object with a messages property
+  }
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    // generate audio file
+    const fileName = `audios/message_${i}.mp3`; // The name of your audio file
+    const textInput = message.text; // The text you wish to convert to speech
+    await voice.textToSpeech(
+      elevenLabsApiKey,
+      voiceIDFemale,
+      fileName,
+      textInput
+    );
+    // generate lipsync
+    await lipSyncMessage(i);
+    message.audio = await audioFileToBase64(fileName);
+    message.lipsync = await readJsonTranscript(`audios/message_${i}.json`);
+  }
+
+  res.send({ messages });
+});
+
+const readJsonTranscript = async (file) => {
+  const data = await fs.readFile(file, "utf8");
+  return JSON.parse(data);
+};
+
+const audioFileToBase64 = async (file) => {
+  const data = await fs.readFile(file);
+  return data.toString("base64");
+};
+
+////////
 
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
